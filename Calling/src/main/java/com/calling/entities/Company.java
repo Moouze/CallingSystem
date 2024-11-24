6d1aefd7840254bfadda3ee3d77561fb528c37c1
Lucas <lucas_dev@outlook.com.br>
2024-11-24 00:17:30 -0300

    add annotation and class relationship

--- Calling/src/main/java/com/calling/entities/Company.java
+++ Calling/src/main/java/com/calling/entities/Company.java
@@ -1,8 +1,19 @@
 package com.calling.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_company")
 public class Company {
@@ -17,5 +28,7 @@ public class Company {
     @NotNull
     private String phone;
 
-
+    @OneToMany(mappedBy = "company")
+    @JsonIgnore
+    private List<Users> usersList;
 }
