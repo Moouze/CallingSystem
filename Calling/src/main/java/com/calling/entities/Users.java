343c05fc451e4913e1bce7e4a3f41cd4348d65f1
Lucas <lucas_dev@outlook.com.br>
2024-11-23 23:33:45 -0300

    update the class Users

--- Calling/src/main/java/com/calling/entities/Users.java
+++ Calling/src/main/java/com/calling/entities/Users.java
@@ -1,7 +1,9 @@
 package com.calling.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +23,15 @@ public class Users {
     private Long id;
     @NotNull
     private String name;
+    @Email(message = "O email deve ser válido")
+    @NotNull
+    @JsonIgnore
+    private String email;
+    @NotNull
+    @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
+    @JsonIgnore
+    private String password;
+
 
     @OneToMany(mappedBy = "users", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     @JsonIgnore
