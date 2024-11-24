d0abd5d4fa37cca86f8f015ad8fcb6d78fd63979
Lucas <lucas_dev@outlook.com.br>
2024-11-24 00:17:55 -0300

    create de crud methods

---
+++ Calling/src/main/java/com/calling/controller/CompanyController.java
@@ -0,0 +1,30 @@
+package com.calling.controller;
+
+import com.calling.entities.Company;
+import com.calling.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @PostMapping
+    public ResponseEntity<Company> createCompany(@RequestBody Company company) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(companyRepository.save(company));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Company>> getAllCompanies() {
+        return ResponseEntity.ok(companyRepository.findAll());
+    }
+
+}
