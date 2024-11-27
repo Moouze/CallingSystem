6f7389f30116769a8972ba3ded9bfa2f66a8d42b
Lucas <lucas_dev@outlook.com.br>
2024-11-27 05:16:10 -0300

    Crud Company

--- Calling/src/main/java/com/calling/controller/CompanyController.java
+++ Calling/src/main/java/com/calling/controller/CompanyController.java
@@ -1,13 +1,17 @@
 package com.calling.controller;
 
+import com.calling.entities.Calling;
 import com.calling.entities.Company;
 import com.calling.repository.CompanyRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/company")
@@ -31,5 +35,20 @@ public class CompanyController {
         return companyRepository.findById(id).map(x -> ResponseEntity.ok(x)).orElse(ResponseEntity.notFound().build());
     }
 
+    @PutMapping
+    public ResponseEntity<Company> updateCompany(@RequestBody @Valid Company company) {
+        return companyRepository.findById(company.getId()).map(x -> ResponseEntity.status(HttpStatus.CREATED).body(companyRepository.save(company))).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCompany(@PathVariable Long id) {
+        Optional<Company> company = companyRepository.findById(id);
+
+        if (company.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        companyRepository.deleteById(id);
+    }
 
 }
