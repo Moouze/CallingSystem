b27bd4ba466851772c2beffb58d0471489a6550c
Lucas <lucas_dev@outlook.com.br>
2024-12-03 13:23:01 -0300

    creation of the login user class

---
+++ Calling/src/main/java/com/calling/entities/UserLogin.java
@@ -0,0 +1,59 @@
+package com.calling.entities;
+
+public class UserLogin {
+
+    private Long id;
+    private String name;
+    private String email;
+    private Role role;
+    private String password;
+    private String token;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return this.role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
